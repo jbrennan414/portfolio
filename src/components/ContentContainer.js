66ba9a167aaffe938344484f9bcc8d6479452f9f
jpb55116@gmail.com <jpb55116@gmail.com>
2019-04-14 10:43:56 -0600

    first commit

---
+++ src/components/ContentContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+export default class ContentContainer extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>THIS WILL BE OUR ContentContainer</h1>
+            </div>
+    );
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>THIS WILL BE OUR HEADER</h1>
+            </div>
+    );
+  }
+}
+
+export default Header;
