cc4cccf2ea35891a0e3f371a08f42a2d826a3f5f
jpb55116@gmail.com <jpb55116@gmail.com>
2019-11-13 19:06:56 -0700

    styling changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,16 +8,18 @@ import Email from '@material-ui/icons/Email'
 class Header extends Component {
     render() {
         return (
-            <div className="header">
-                <h1>JOHN BRENNAN</h1>
-                <div className="contactIcons">
+            <div className="firstHeader">
+                <div className="headerText">
+                    <h1>JOHN BRENNAN</h1>
+                    <h4>fullstack developer</h4>
+                </div>
+                <div id="icons">
                     <a href={"https://github.com/jbrennan414"}><GitHub /></a>
                     <a href={"https://twitter.com/jbrennan414"}><Twitter /></a>
                     <br />
                     <a href={"https://www.linkedin.com/in/jbrennan414/"}><LinkedIn /></a>
                     <a href="mailto:jpb55116@gmail.com"><Email /></a>
                 </div>
-                <h4>fullstack developer</h4>
             </div>
     );
   }
