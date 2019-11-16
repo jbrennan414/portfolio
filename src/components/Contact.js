a0ec6a86004916e8cc91e64fbfb7f30c7d63e339
jpb55116@gmail.com <jpb55116@gmail.com>
2019-11-15 19:44:30 -0700

    styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ export default class Contact extends Component {
         return (
             <div className="header">
                 <h1>CONTACT</h1>
+                <p>FOOOOO SWAG</p>
             </div>
     );
   }
